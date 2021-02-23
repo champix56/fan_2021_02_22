fa5cab2887813fead48574f33a1e9a10ff8643fd
alex <desorbaix@free.fr>
2021-02-23 14:33:39 +0100

    mise en place d'un get http avec affichage en vue

--- app/produitsCtrl/produitsCtrl.js
+++ app/produitsCtrl/produitsCtrl.js
@@ -5,18 +5,36 @@
         .module('app')
         .controller('produitsCtrl', ProduitController);
 
-    ProduitController.$inject = [];
-    function ProduitController() {
+    ProduitController.$inject = ['$http'];
+    function ProduitController($http) {
         var vm = this;
-        this.produits=[
-            {id:0,nom:'beurre doux normand', description:'beurre sans sel pas bon',img:'' },
-            {id:1,nom:'beurre Breton salé 5%', description:'le meilleur',img:'' }
+        this.produits = [
+            { id: 0, nom: 'beurre doux normand', description: 'beurre sans sel pas bon', img: '' },
+            { id: 1, nom: 'beurre Breton salé 5%', description: 'le meilleur', img: '' }
         ]
 
         activate();
 
         ////////////////
 
-        function activate() { }
+        function activate() {
+            $http({
+                url: 'http://localhost:5629/produits?_expand=categorie&_sort=categorieId',
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+                .then(
+                    function (response) {
+                        vm.produits = response.data;
+                        console.log('%c%s','font-size:32pt; color: green;font-weight:900','produits chargés dans le ctrl');
+                        console.log(vm.produits);
+                    },
+                    function (response) {
+                        console.log('Produits REST fail:' + response.satus)
+                    }
+                );
+        }
     }
 })();
